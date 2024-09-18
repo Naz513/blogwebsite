98e10dd6c6b3ec758bd89130039eb4de063651a2
Naz513 <nsaquib96@gmail.com>
2024-09-18 00:36:46 -0400

    Fixed Typescript data

--- src/consts.ts
+++ src/consts.ts
@@ -1,16 +1,13 @@
-import type { Site, Info, Metadata, Socials } from "@types";
+import type { Site, Metadata, Socials } from "@types";
 
 export const SITE: Site = {
   NAME: "Mohd Saquib",
   EMAIL: "nsaquib96@gmail.com",
+  TITLE:"Cloud Solutions Architect",
+  LOCATION:"Virginia, US",
   NUM_POSTS_ON_HOMEPAGE: 3,
   NUM_WORKS_ON_HOMEPAGE: 2,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
-};
-
-export const INFO: Info = {
-  LOCATION: "Virginia, US",
-  TITLE: "Cloud Solutions Architect",
 };
 
 export const HOME: Metadata = {
--- src/types.ts
+++ src/types.ts
@@ -1,14 +1,11 @@
 export type Site = {
   NAME: string;
   EMAIL: string;
+  TITLE: String;
+  LOCATION: string;
   NUM_POSTS_ON_HOMEPAGE: number;
   NUM_WORKS_ON_HOMEPAGE: number;
   NUM_PROJECTS_ON_HOMEPAGE: number;
-};
-
-export type Info = {
-  LOCATION: string;
-  TITLE: string;
 };
 
 export type Metadata = {
